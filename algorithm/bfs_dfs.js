690f75f305de49f4414c87afd460c932db7ac94a
Lee Jeongmin <ljm5937@osstem.com>
2023-02-13 16:56:09 +0900

    [javascript] 순열과 조합

--- algorithm/bfs_dfs.js
+++ algorithm/bfs_dfs.js
@@ -28,3 +28,31 @@ const BFS = (graph, startNode) => {
   return visited;
 };
 console.log(BFS(graph, "A"));
+
+
+//////////////////////////////////////////////////////////////////////
+let N = 5;
+let M = 5;
+let graph = new Array(N + 1).fill(0, 0, N + 1);
+for (let i = 0; i <= N + 1; i++){
+  graph[i] = new Array(N + 1).fill(0, 0, N + 1);
+}
+console.log(graph)
+
+const BFS = (graph, startNode) => {
+  const visited = []; // 탐색을 마친 노드들
+  let needVisit = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+  while (needVisit.length !== 0) {
+    // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    console.log(needVisit);
+    if (!visited.includes(node)) {
+      // 해당 노드가 탐색된 적 없다면
+      visited.push(node);
+      needVisit = [...needVisit, ...graph[node]];
+    }
+  }
+  return visited;
+};
