07272bc492089f644bfb3a1ea0aad1a684b4f192
Lee Jeongmin <ljm5937@osstem.com>
2023-02-13 16:55:57 +0900

    [javascript] 정렬

--- u-javascript/section-12/selection_sort.js
+++ u-javascript/section-12/selection_sort.js
@@ -1,5 +1,5 @@
 // LEGACY VERSION (non ES2015 syntax)
-function sselectionSort(arr){
+function selectionSort(arr){
     for(var i = 0; i < arr.length; i++){
         var lowest = i;
         for(var j = i+1; j < arr.length; j++){
--- u-javascript/section-13/insertion_sort.js
+++ u-javascript/section-13/insertion_sort.js
@@ -1,13 +1,15 @@
-function insertionSort(arr){
-	var currentVal;
-    for(var i = 1; i < arr.length; i++){
-        currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j+1] = arr[j]
-        }
-        arr[j+1] = currentVal;
+//삽입정렬
+function insertionSort(arr) {
+  var currentVal; //현재값 변수 선언
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i]; //1->9->76->4
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+      console.log("i:",i," j:",j)
     }
-    return arr;
+    arr[j + 1] = currentVal;
+  }
+  return arr;
 }
 
-insertionSort([2,1,9,76,4])
+insertionSort([2, 1, 9, 76, 4]);
