45d20f74d4896cfa008d44d295104795c0cf9fb7
Lee Jeongmin <ljm5937@osstem.com>
2023-02-15 09:34:13 +0900

    폴더 구조 변경

--- javascript-algorithm/combination.js
+++ javascript-algorithm/combination.js
@@ -3,7 +3,10 @@ const getCombinations = function (arr, selectNumber) {
   if (selectNumber === 1) return arr.map(el => [el]);
   // n개중에서 1개 선택할 때(nC1), 바로 모든 배열의 원소 return
 
-  arr.forEach((fixed, index, origin) => {
+    arr.forEach((fixed, index, origin) => {
+        console.log(fixed);
+        console.log(index);
+        console.log(origin);
     const rest = origin.slice(index + 1);
     // 해당하는 fixed를 제외한 나머지 뒤
     const combinations = getCombinations(rest, selectNumber - 1);
@@ -16,3 +19,21 @@ const getCombinations = function (arr, selectNumber) {
 
   return results; // 결과 담긴 results return
 };
+
+console.log(getCombinations([1, 2, 3], 2));
+
+const getPermutaions = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map(el => [el]);
+
+    arr.forEach((fixed, index, origin) => {
+        const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+        const permutations = getPermutaions(rest, selectNumber - 1);
+        const attached = permutations.map(el => [fixed, ...el]);
+
+        results.push(...attached);
+    });
+    return results;
+}
+
+console.log(getPermutaions([1,2,3,4],3))
